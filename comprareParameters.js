872832ebd75df3bbb4744c91f10147a657d55016
Roberto Carlos da Silva Junior <48263907+rcsjunior1987@users.noreply.github.com>
2020-03-16 15:13:58 +1000

    Create comprareParameters.js

---
+++ comprareParameters.js
@@ -0,0 +1,7 @@
+// Write a simple JavaScript function that takes two parameters and compares them considering their VALUE and TYPE, returning the boolean result of this comparison
+function ComprareParameters(Param1, Param2) {
+    return (Param1 === Param2);
+}
+
+var result = ComprareParameters(10, 10);
+console.log(result);
